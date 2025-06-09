688ad9dc115d6f7929f508b2f3a737424f36b61c
Junseo1026 <plot1123@hanmail.net>
2025-06-09 15:26:19 +0900

    audio frontend

--- react-app/src/components/Bottombar.jsx
+++ react-app/src/components/Bottombar.jsx
@@ -3,23 +3,19 @@
 import React from 'react'
 import '../css/Bottombar.css'
 
-export default function BottomBar({
-  statusText,
-  onRecordClick,
-  onSummarizeClick,
-  onUploadClick,
-  onOcrClick      // 추가된 prop
-}) {
+export default function BottomBar({ statusText, isRecording, onRecordClick, onSummarizeClick, onUploadClick, onOcrClick }) {
   return (
     <footer className="bottom-bar">
       <div className="bottom-status">
         {statusText && <span>🔵 {statusText}</span>}
       </div>
       <div className="bottom-actions">
-        <button onClick={onRecordClick}>녹음</button>
+        <button onClick={onRecordClick}>
+          {isRecording ? '녹음 종료' : '녹음'}
+        </button>
         <button onClick={onSummarizeClick}>요약</button>
         <button onClick={onUploadClick}>업로드</button>
-        <button onClick={onOcrClick}>텍스트 변환</button> {/* 새 버튼 */}
+        <button onClick={onOcrClick}>텍스트 변환</button>
       </div>
     </footer>
   )
